ce3df0c61c533f0eefb3f1c3c02920423ba2ed19
zooloo <lowdog136@gmail.com>
2022-01-08 14:09:57 +0200

    правки в тексте

--- src/views/AboutClub.vue
+++ src/views/AboutClub.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
-    <img alt="ФК Север логотип" width="150" height="150" src="../assets/fcsever_logo.png">
+    <img alt="ФК Север логотип" width="200" height="200" src="../assets/fcsever_logo.png">
     <MainBlock msg="Добро пожаловать на сайт SeverFans"/>
   </div>
   <div class="AboutClub">
-    <h1>This is an about page for club1</h1>
+    <h1>Страница с историей о футбольном клубе "Север" г.Мурманск</h1>
   </div>
 </template>
