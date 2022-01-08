ba05b510ee71cf4f7e7dea2d1bf1ac2d8c44e70f
zooloo <lowdog136@gmail.com>
2022-01-08 14:02:34 +0200

    images logo add

--- src/views/AboutFans.vue
+++ src/views/AboutFans.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="AboutFans">
-    <h1>This is an about page for fans</h1>
+  <div class="home">
+    <img alt="SSM logo" width="150" height="150" src="../assets/images/ssm_logo.jpg">
+    <MainBlock msg="Добро пожаловать на сайт SeverFans"/>
+  </div>
+  <div class="AboutClub">
+    <h1>This is an about page for club1</h1>
   </div>
 </template>
