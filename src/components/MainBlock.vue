347050164a3cd532987c19eff0a780b9206f6075
zooloo <lowdog136@gmail.com>
2022-01-09 19:16:42 +0200

    Добавление Footer.vue

--- src/components/MainBlock.vue
+++ src/components/MainBlock.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Footer',
   props: {
     msg: String
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.jpg">
     <MainBlock msg="Добро пожаловать на сайт SeverFans"/>
+  </div>;
+  <div id="footer">
+    <Footer msg="Создан в 2007" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import MainBlock from '@/components/MainBlock'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'Home',
   components: {
-    MainBlock
+    MainBlock, Footer
   }
 }
 </script>
