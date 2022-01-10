d66e58f7ea6a439858cfbc7edd2c731c4e67c9f3
zooloo <lowdog136@gmail.com>
2022-01-10 20:13:45 +0200

    Работа над добавлением фото

--- src/views/Foto.vue
+++ src/views/Foto.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="AboutClub">
     <h3>Страница с фото</h3>
-    <img width="450" height="330" src="../assets/images/ultra/2013/11-08/1.jpg"> <br>
-    <img width="450" height="330" src="../assets/images/ultra/2013/11-08/2.jpg">
   </div>
+<!--  <select v-model="filter">-->
+<!--    <option value="foto_2011"> 2011 год</option>-->
+<!--    <option value="foto_2012"> 2012 год</option>-->
+<!--    <option value="foto_2013"> 2013 год</option>-->
+<!--  </select>-->
+<!--  <br>-->
+<!--  <br>-->
+  <img width="450" height="330" src="../assets/images/ultra/2013/11-08/1.jpg"> <br>
+  <img width="450" height="330" src="../assets/images/ultra/2013/11-08/2.jpg">
 </template>
+<script>
+export default {
+  data () {
+    return {
+      filter: '2013'
+    }
+  }
+  // watch: {
+  //   filter (value) {
+  //     console.log(value)
+  //   }
+  // }
+}
+</script>
