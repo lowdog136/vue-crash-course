de35beb61900eab34b8d8d86b134f8afad09a3b1
Denis Khodarev <lowdog136@gmail.com>
2022-02-11 12:49:29 +0200

    Правка содердимого страницы

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="ssm logo" src="../assets/logo.jpg">
-    <h5>Связаться со мной можно через email: <br>
-      lowdogодинтришестьсобакагмайлдотком</h5>
+  <div class="app">
+    <PictureInTopAll />
+    <h5>Cвязаться со мной </h5>
+    <div id="content" style="text-align: left">
+      Напишите мне на e-mail: lowdog{одинтришесть}@gmail.com <br>
+      Я обязательно отвечу.<br>
+      Приятного вам дня.
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import PictureInTopAll from '@/components/PictureInTopAll'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    PictureInTopAll
+  }
 }
 </script>
+
+<style>
+#content {
+  margin-left: auto;
+  margin-right: auto;
+  width: 40em
+}
+</style>
