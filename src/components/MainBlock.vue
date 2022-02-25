0a0ac90a18205f5878ab84a342539f3292bcb120
Denis Khodarev <lowdog136@gmail.com>
2022-02-25 09:49:27 +0200

    Работа с внешним видом главной страницы.

--- src/components/MainBlock.vue
+++ src/components/MainBlock.vue
@@ -8,7 +8,7 @@
     </div>
   </div>
   <div class="mainimage">
-    <img width="900" height="530" src="../assets/images/stad2card.jpg">
+    <img width="800" height="530" src="../assets/images/stad2card.jpg">
   </div>
 </template>
 
