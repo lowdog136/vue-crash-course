614f6e6c064622a6e0849e7151f8c4ce30a605b6
Denis Khodarev <lowdog136@gmail.com>
2022-03-01 13:03:01 +0200

    Установка YandexMetrika

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueYandexMetrika from 'vue-yandex-metrika'
 
 createApp(App).use(router).mount('#app')
+createApp(VueYandexMetrika, {
+  id: 30739543,
+  router: router,
+  env: process.env.NODE_ENV
+  // other options
+})
